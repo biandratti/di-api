697b23a2440988abb4df465b48dab077254df334
Biandratti <maxibiandra@gmail.com>
2024-02-05 19:47:53 +0100

    get API done

--- src/infrastructure/mongo.rs
+++ src/infrastructure/mongo.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
-
 use bson::DateTime;
+use futures_util::stream::StreamExt;
 use log::info;
 use mongodb::{Client, Collection, Database};
+use std::error::Error;
 
 use crate::domain::repositories::FingerprintRepository;
 
@@ -31,13 +31,17 @@ impl FingerprintRepository for MongoFingerprintRepository {
         fingerprint: &mut Fingerprint,
     ) -> Result<(), Box<dyn Error + Send + Sync>> {
         fingerprint.created = Some(DateTime::now());
-        //fingerprint.id = Uuid::new();
         self.collection.insert_one(fingerprint, None).await?;
         Ok(())
     }
 
-    /*async fn get_all(&self) -> Result<Vec<Fingerprint>, Box<dyn Error + Send + Sync>> {
-        self.collection.find(None, None).await?;
-        Ok(Vec::new())
-    }*/
+    async fn get_all(&self) -> Result<Vec<Fingerprint>, Box<dyn Error + Send + Sync>> {
+        let mut fingerprints = Vec::new();
+        let mut cursor = self.collection.find(None, None).await?;
+        while let Some(doc) = cursor.next().await {
+            fingerprints.push(doc?);
+        }
+
+        Ok(fingerprints)
+    }
 }
--- src/presentation/routes.rs
+++ src/presentation/routes.rs
@@ -1,8 +1,10 @@
+use log::error;
 use warp::Filter;
 
 use crate::domain::entities::Fingerprint;
 use crate::domain::use_cases::FingerprintUseCase;
 use crate::infrastructure;
+
 #[derive(Debug)]
 struct CustomRejection(Box<dyn std::error::Error + Send + Sync>);
 
@@ -11,7 +13,7 @@ impl warp::reject::Reject for CustomRejection {}
 pub fn routes(
     repo: infrastructure::mongo::MongoFingerprintRepository,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    fingerprint_post(repo)
+    fingerprint_post(repo.clone()).or(fingerprint_get_all(repo.clone()))
 }
 
 fn fingerprint_post(
@@ -27,7 +29,27 @@ fn fingerprint_post(
                 match use_case.create_fingerprint(&mut fingerprint).await {
                     Ok(()) => Ok(warp::reply()),
                     Err(e) => {
-                        eprintln!("Error creating fingerprint: {}", e);
+                        error!("Error creating fingerprint: {}", e);
+                        Err(warp::reject::custom(CustomRejection(e)))
+                    }
+                }
+            }
+        })
+}
+
+fn fingerprint_get_all(
+    repo: infrastructure::mongo::MongoFingerprintRepository,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::get()
+        .and(warp::path("fingerprint"))
+        .and(warp::path::end())
+        .and_then(move || {
+            let use_case = repo.clone(); // Use your repository instance
+            async move {
+                match use_case.get_all_fingerprints().await {
+                    Ok(fingerprint_list) => Ok(warp::reply::json(&fingerprint_list)),
+                    Err(e) => {
+                        error!("Error getting all fingerprints: {}", e);
                         Err(warp::reject::custom(CustomRejection(e)))
                     }
                 }
