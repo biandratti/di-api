f911ebb9ad1e66a2c0b3bf9f58e24e5619b3c7fc
Biandratti <maxibiandra@gmail.com>
2024-02-06 09:59:45 +0100

    swagger server created

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use std::sync::Arc;
-
 use utoipa::OpenApi;
 use utoipa_swagger_ui::Config;
-use warp::{
-    http::Uri,
-    hyper::{Response, StatusCode},
-    path::{FullPath, Tail},
-    Filter, Rejection, Reply,
-};
+use warp::Filter;
 
 use crate::domain::entities::Fingerprint;
 use crate::presentation::routes;
 
 mod domain;
+mod http_utils;
 mod infrastructure;
 mod presentation;
 
@@ -43,50 +38,16 @@ async fn main() {
         .and(warp::path::full())
         .and(warp::path::tail())
         .and(warp::any().map(move || config.clone()))
-        .and_then(serve_swagger);
+        .and_then(http_utils::serve_swagger::serve_swagger);
 
     let repo: infrastructure::mongo::MongoFingerprintRepository =
         infrastructure::mongo::MongoFingerprintRepository::new("mongodb://localhost:27017")
             .await
             .unwrap();
 
-    //TODO: WIP
-    let _routes = presentation::routes::routes(repo);
+    let routes = presentation::routes::routes(repo);
 
-    warp::serve(api_doc.or(swagger_ui))
-        //.or(routes)
+    warp::serve(api_doc.or(swagger_ui).or(routes))
         .run(([127, 0, 0, 1], 8080))
         .await;
-}
-
-async fn serve_swagger(
-    full_path: FullPath,
-    tail: Tail,
-    config: Arc<Config<'static>>,
-) -> Result<Box<dyn Reply + 'static>, Rejection> {
-    if full_path.as_str() == "/swagger-ui" {
-        return Ok(Box::new(warp::redirect::found(Uri::from_static(
-            "/swagger-ui/",
-        ))));
-    }
-
-    let path = tail.as_str();
-    match utoipa_swagger_ui::serve(path, config) {
-        Ok(file) => {
-            if let Some(file) = file {
-                Ok(Box::new(
-                    Response::builder()
-                        .header("Content-Type", file.content_type)
-                        .body(file.bytes),
-                ))
-            } else {
-                Ok(Box::new(StatusCode::NOT_FOUND))
-            }
-        }
-        Err(error) => Ok(Box::new(
-            Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(error.to_string()),
-        )),
-    }
 }
