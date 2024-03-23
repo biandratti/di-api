54d75e1cf303e13c51124ec92bf9b4746daab603
Biandratti <maxibiandra@gmail.com>
2024-03-23 21:05:43 +0100

    test fixed

--- tests/integration_tests/test_fingerprint.rs
+++ tests/integration_tests/test_fingerprint.rs
@@ -11,25 +11,21 @@ use fingerprint_api::infrastructure::mongo::MongoClient;
 #[test]
 async fn it_get_fingerprints() {
     dotenv::from_filename(".env.test").ok();
-    let db_name = dotenv::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
+    let db_name = &dotenv::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
     let docker = clients::Cli::default();
     let msg = WaitFor::message_on_stdout("server is ready");
-    // let port = 27018;
+    let port: u16 = 27017;
     let generic = GenericImage::new("mongo", "6.0.7")
         .with_wait_for(msg.clone())
-        .with_env_var(
-            "MONGO_INITDB_DATABASE",
-            dotenv::var("DATABASE_NAME").expect("DATABASE_NAME must be set"),
-        )
-        //        .with_env_var("MONGO_INITDB_ROOT_USERNAME", "root")
-        //        .with_env_var("MONGO_INITDB_ROOT_PASSWORD", "root")
-        .with_exposed_port(27017);
+        .with_env_var("MONGO_INITDB_DATABASE", db_name)
+        .with_env_var("MONGO_INITDB_ROOT_USERNAME", "root")
+        .with_env_var("MONGO_INITDB_ROOT_PASSWORD", "root")
+        .with_exposed_port(port);
 
     let node = docker.run(generic);
-    let port = node.get_host_port_ipv4(27017);
-    println!("Port: {}", port);
+    let port = node.get_host_port_ipv4(port);
 
-    let db_url = format!("mongodb://localhost:{}", port); //format!("mongodb://root:root@localhost:{}", port);
+    let db_url = format!("mongodb://root:root@localhost:{}", port);
     let mongo_client: MongoClient = MongoClient::new(&db_url).await.unwrap();
     let repo: MongoFingerprintRepository =
         MongoFingerprintRepository::new(mongo_client.client, &db_name)
