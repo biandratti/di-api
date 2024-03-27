b9e47115e7ea52b55fb016b0cdcc54e0833ce4c1
Biandratti <maxibiandra@gmail.com>
2024-03-27 15:36:08 +0100

    setting open telemetry

---
+++ src/adapters/api/metrics/metrics_controller.rs
@@ -0,0 +1,31 @@
+use std::sync::Arc;
+
+use once_cell::sync::Lazy;
+use opentelemetry::KeyValue;
+use prometheus::{Encoder, TextEncoder};
+use warp::Filter;
+
+use crate::adapters::api::metrics::app_state::AppState;
+
+pub fn build(state: Arc<AppState>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    metrics(state);
+}
+
+//TODO: move...
+static HANDLER_ALL: Lazy<[KeyValue; 1]> = Lazy::new(|| [KeyValue::new("handler", "all")]);
+
+fn metrics(state: Arc<AppState>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::get().and(warp::path("metrics")).map({
+        let state = state.clone();
+        move || metrics_handler(state.clone())
+    })
+}
+
+async fn metrics_handler(state: Arc<AppState>) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut buffer = vec![];
+    let encoder = TextEncoder::new();
+    let metric_families = state.registry.gather();
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    state.http_body_gauge.record(buffer.len() as u64, HANDLER_ALL.as_ref());
+    Ok(warp::reply::with_header(buffer, "content-type", "text/plain"))
+}
